b0b38b695a9d866b8fa236769429c0c742792025
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2023-10-10 23:00:54 -0400

    Working JDBC connection

--- JDBC_Proj/src/ConnectURL.java
+++ JDBC_Proj/src/ConnectURL.java
@@ -1,5 +1,3 @@
-package JDBC_Project.src;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,24 +5,27 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ConnectURL {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         // Create a variable for the connection string
-        String connectionUrl = "jdbc:sqlserver://localhost:13002;databaseName=AdventureWorks;user=sa;password=PH@123456789";
+        String connectionUrl = "jdbc:sqlserver://localhost:13002;databaseName=TSQLV4;user=sa;password=PH@123456789;encrypt=true;trustServerCertificate=true";
 
         try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
 
-            String SQL = "SELECT TOP 10 * FROM Person.Contact";
+            String SQL = "SELECT TOP 10 * FROM Sales.Orders";
             ResultSet rs = stmt.executeQuery(SQL);
 
             // Iterate through the data in the result set and display it
             while (rs.next()) {
-                System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));
+                System.out.println(rs.getString("custid"));
             }
         }
+
         // Handle any errors that may have occered
         catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
